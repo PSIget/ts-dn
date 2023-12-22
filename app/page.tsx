0cf3858a62342790b7c6ab15af40efb94d9b93bb
PSIget <psi@s2ue.org>
2023-12-23 01:25:35 +0700

    Fiz colors

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       </div> */}
       <div></div>
 
-      <div className="relative flex flex-col gap-8 place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-[#a77e3d] before:dark:opacity-10 after:dark:from-[#a77e3d] after:dark:via-[#a77e3d] after:dark:opacity-40 before:lg:h-[360px]">
+      <div className="relative flex flex-col gap-8 place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:blur-2xl after:content-[''] before:bg-gradient-to-br before:from-transparent before:to-[#a77e3d] before:opacity-10 after:from-[#a77e3d] after:via-[#a77e3d] after:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative !dark:invert"
           src="/TS-Logo-Whitecs.png"
